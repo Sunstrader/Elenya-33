840b19a70891946f05b7e47d39b4e84b00902e55
Sunstrader <43101106+Sunstrader@users.noreply.github.com>
2026-09-17 10:53:41 +0200

    test(ng+): load ultra visual layer

--- elenya-refonte-52.4/qa/engine.cjs
+++ elenya-refonte-52.4/qa/engine.cjs
@@ -9,7 +9,7 @@ function loadEngine(root, customProps) {
   const files = ['20_Core_Helpers.js','21_Assets.js','22_State.js','10_Triggers.js',
     'DATABASE_Acte1.js','DATABASE_Acte2.js','DATABASE_Acte3.js','DATABASE_Acte4.js','DATABASE_Acte5.js',
     'DATABASE_Relations.js','DATABASE_Fragments.js','DATABASE_Secondary.js','DATABASE_Endings.js','DATABASE_Misc.js',
-    'DATABASE_Index.js','DATABASE_NGPlus.js','23_Refonte.js','24_NGPlus_Ultra.js','30_SceneManager.js','50_Server_API.js','90_QA.js','95_NGPlus_Ultra_QA.js'];
+    'DATABASE_Index.js','DATABASE_NGPlus.js','23_Refonte.js','24_NGPlus_Ultra.js','25_NGPlus_Ultra_Visuals.js','30_SceneManager.js','50_Server_API.js','90_QA.js','95_NGPlus_Ultra_QA.js'];
   for(const f of files) if(fs.existsSync(path.join(root,f))) vm.runInContext(fs.readFileSync(path.join(root,f),'utf8'),ctx,{filename:f});
   vm.runInContext('if(typeof applyRefonte_==="function")applyRefonte_(); globalThis.api = {DB, NGPLUS_DB, SceneManager, normalizeGameState, serverGetPendingScene, serverProcessChoice, serverValidateDatabase, BUILD_VERSION, ELENYA_ENDING_VIDEO_IDS_V55};',ctx);
   return {ctx,api:ctx.api,props};
@@ -20,7 +20,7 @@ function createIsolatedEngine(root) {
   const files = ['20_Core_Helpers.js','21_Assets.js','22_State.js','10_Triggers.js',
     'DATABASE_Acte1.js','DATABASE_Acte2.js','DATABASE_Acte3.js','DATABASE_Acte4.js','DATABASE_Acte5.js',
     'DATABASE_Relations.js','DATABASE_Fragments.js','DATABASE_Secondary.js','DATABASE_Endings.js','DATABASE_Misc.js',
-    'DATABASE_Index.js','DATABASE_NGPlus.js','23_Refonte.js','24_NGPlus_Ultra.js','30_SceneManager.js','50_Server_API.js','95_NGPlus_Ultra_QA.js'];
+    'DATABASE_Index.js','DATABASE_NGPlus.js','23_Refonte.js','24_NGPlus_Ultra.js','25_NGPlus_Ultra_Visuals.js','30_SceneManager.js','50_Server_API.js','95_NGPlus_Ultra_QA.js'];
   const compiled = files.filter(f=>fs.existsSync(path.join(root,f))).map(f=>new vm.Script(fs.readFileSync(path.join(root,f),'utf8'),{filename:f}));
   const initScript = new vm.Script('if(typeof applyRefonte_==="function")applyRefonte_();');
   // Charge une instance de référence pour les métadonnées statiques immuables (catalogue scènes, fins, musiques)
